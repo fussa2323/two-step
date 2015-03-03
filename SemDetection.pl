a62d4d18f9c3c00a28f53e09dbdb45f281c3df15
Yohei Arikilab <yoohei2323@gmail.com>
2015-03-03 17:52:45 +0900

    Modify SemCorrection SemDeteciton

--- SemDetection.pl
+++ SemDetection.pl
@@ -5,7 +5,7 @@
 $testdir = "test_data";
 $outdir = "out_back";
 
-$model = "ContextModel/Semmodel";
+$model = "ContextModel/Semmodel_csjnrd";
 
 opendir(TESTDIR,"$testdir");
 @testfile = readdir(TESTDIR);
